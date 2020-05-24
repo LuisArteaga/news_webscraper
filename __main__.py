e102223bfc39b990e335d0c4c5043d31ce0da954
Luis Arteaga <arteaga@live.de>
2020-05-24 16:50:45 +0200

    final code

--- __main__.py
+++ __main__.py
@@ -2,41 +2,49 @@ import ntpath
 from selenium import webdriver as webdriver
 from selenium.webdriver.common.by import By
 import re
+import csv
+
 
 class Configuration:
-    def __init__(self, driver='./driver/chromedriver.exe'):
-        self.driver = ntpath.normpath(driver)
+    """
+    This class contains the information about the chrome driver and the details about website to be crawled
+    """
+
+    def __init__(self, chrome_driver='./driver/chromedriver.exe'):
+        self.driver = ntpath.normpath(chrome_driver)
+        self.websites = {'finanzen.net': {'stocks': ['adidas',
+                                                     'allianz',
+                                                     'beiersdorf',
+                                                     'deutsche_bank',
+                                                     'deutsche_telekom',
+                                                     'lufthansa',
+                                                     'siemens',
+                                                     'volkswagen'
+                                                     'vonovia',
+                                                     'wirecard'
+                                                     ],
+                                          'base_url': 'https://www.finanzen.net/news/de/',
+                                          'url_parameters': '-news@intpagenr_'}}
 
 
 configuration = Configuration()
-URL = 'https://www.finanzen.net/news/de/adidas-news@intpagenr_13'
 driver = configuration.driver
 driver = webdriver.Chrome(executable_path=driver)
-driver.get(URL)
 
-table_news = driver.find_element(By.XPATH, "//table[@class='table news-list']")
-tr = table_news.find_elements(By.XPATH, '//tr')
-re_date = r'[0-9]{2}\.[0-9]{2}\.[0-9]{2}\n'
+BASE_URL = configuration.websites['finanzen.net']['base_url']
 
-for index, element in enumerate(tr):
-    if re.match(re_date, element.text):
-        print(repr(element.text).splitlines())
+for stock in configuration.websites['finanzen.net']['stocks']:
+    url = BASE_URL + stock + configuration.websites['finanzen.net']['url_parameters']
+    for i in range(1, 26):
+        driver.get(url+str(i))
+        driver.implicitly_wait(10)
+        re_date = r'[0-9]{2}\.[0-9]{2}\.[0-9]{2}\n'
+        table_news = driver.find_element(By.XPATH, "//table[@class='table news-list']")
+        tr = table_news.find_elements(By.XPATH, '//tr')
 
-
-
-
-# <table class="table news-list">
-# ->    <tbody>
-# -->   <tr> (1)
-# --->  <td> (1.1)
-# ----> <div> (1.1.1) DATE
-# --->  <td> (1.3)
-# ----> <a class="teaser" ...> Titel
-
-# --> <tr> (?)
-# ---> <td colspan="3"> (?.1)
-# ----> <span class="teaser-headline">
-# -----> <a href="...">Title </a>
-# Date fehlt
-
-
+        with open('./export/{}-{}.csv'.format(stock, str(i)), 'w', newline='') as file:
+            writer = csv.writer(file)
+            for index, element in enumerate(tr):
+                if re.match(re_date, element.text):
+                    writer.writerow(
+                        re.sub(string=repr(element.text), pattern='(^\'|\'$|^"|"$)', repl='').split(sep='\\n'))
